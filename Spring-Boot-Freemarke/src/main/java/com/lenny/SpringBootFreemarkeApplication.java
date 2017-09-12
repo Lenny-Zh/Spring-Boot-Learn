31435e70e733f5bf33c878af1c18ebc94d2275b5
JarlyZly <jarly.zhong@outlook.com>
2017-09-12 21:17:28 +0800

    add sb freemarket

---
+++ Spring-Boot-Freemarke/src/main/java/com/lenny/SpringBootFreemarkeApplication.java
@@ -0,0 +1,12 @@
+package com.lenny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFreemarkeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFreemarkeApplication.class, args);
+    }
+}
