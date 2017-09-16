d113bcc3279ab59d0db62000f3e00ab547a6f2cb
Lenny-Zh <lenny.z@foxmail.com>
2017-09-16 21:07:27 +0800

    add cache Qiniu

--- Spring-Boot-Qiniu/src/main/java/com/lenny/qiniu/config/QiniuServer.java
+++ Spring-Boot-Qiniu/src/main/java/com/lenny/qiniu/config/QiniuServer.java
@@ -17,8 +17,11 @@ import org.springframework.beans.factory.annotation.Value;
 public class QiniuServer {
 
     private String bucket="learnly";
+
     private String accessKey = "qdHsooyPTF--AoDFT9i4gvbVN-T8DSbu3EVlKs3E";
+
     private String secretKey = "h7Aoy_QzK6_6ck9MjfAx8MBuj8Rd9AJ2U-jTfc_m";
+
     private long expireSeconds = 1 * 60 * 60 * 1000;
     private final String QINIU_CDN = "http://owd5a52qv.bkt.clouddn.com/";
     private  String token = null;
